68670e98c59018a36f1386c90297e725bad7687f
Peter Pouliot <ppouliot@gmail.com>
2013-10-17 09:36:43 -0400

    changing filename

--- manifests/pxefile.pp
+++ manifests/pxefile.pp
@@ -2,11 +2,12 @@ class basenode::pxefile {
 
   $arp_type = '01'
 
-  $host_macaddress = regsubst($macaddress, '(\:)','(\-)','G')
+  #$host_macaddress = regsubst($macaddress, '(\:)','(\-)','G')
+  $host_macaddress = regsubst($macaddress, '(\:)','-','G')
 
 
 
-  file {"/nfs/${hostname}/${arp_type}${host_macaddress}":
+  file {"/nfs/${hostname}/${arp_type}-${host_macaddress}":
     ensure => file,
     content => template('basenode/pxefile.erb'),
     require => File["/nfs/${hostname}"],
