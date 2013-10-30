c701072b5d020153a67bdc61cc08f83d725fd0fc
Peter Pouliot <ppouliot@gmail.com>
2013-10-30 13:11:29 -0400

    confining facts to only run on linux

--- lib/facter/default_gateway.rb
+++ lib/facter/default_gateway.rb
@@ -1,5 +1,6 @@
 require 'facter'
 Facter.add("default_gateway") do
+  confine :kernel => "Linux"
   setcode do
   Facter::Util::Resolution.exec( "netstat -nr |grep UG |awk '{ print $2}'" )
   end
--- lib/facter/homedir.rb
+++ lib/facter/homedir.rb
@@ -1,5 +1,6 @@
 require 'facter'
 Facter.add("homedir") do
+  confine :kernel => "Linux"
   setcode do
   Facter::Util::Resolution.exec( "echo $HOME")
   end
